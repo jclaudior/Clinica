4c2a948c4cc7e1a74054350f71f80d2c4353eb45
João Claudio Ribeiro <joaoclaudiodiva@gmail.com>
2019-08-23 12:07:32 -0300

    Criação-do-Index/pasta-compiles-css

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="node_modules/bootstrap/compiler/bootstrap.css">
+        <link rel="stylesheet" href="node_modules/bootstrap/compiler/style.css">
+    </head>
+    <body>
+        <script src="node_modules/jquery/jquery.js"></script>
+        <script src="node_modules/popper.js/dist/umd/popper.js"></script>
+        <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+    </body>
+</html>
